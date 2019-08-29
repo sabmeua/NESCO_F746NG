e1feaa16ead3b3839366f2fc284e7634856b77c1
sabmeua <sabme.ua@gmail.com>
2019-08-29 03:53:23 +0000

    Add comments

--- nesco/include/Cpu.h
+++ nesco/include/Cpu.h
@@ -6,6 +6,21 @@
 
 namespace nesco
 {
+    /**
+     * Memory map
+     *
+     * $0000 - $07FF	RAM
+     * $0800 - $1FFF	Unused (mirrored $0000-$07FF)
+     * $2000 - $2007	I/O (PPU)
+     * $2008 - $3FFF	Unused (mirrored $2000-$2007)
+     * $4000 - $401F	I/O (APU, etc)
+     * $4020 - $5FFF	Extented RAM (for specific mapper)
+     * $6000 - $7FFF    Battery backuped NVRAM
+     * $8000 - $BFFF	Program ROM-Low
+     * $C000 - $FFFF	Program ROM-High
+     *
+     */
+
     class Cpu
     {
     public:
@@ -21,11 +36,11 @@ namespace nesco
 
     private:
         // Registers
-        uint8_t A;
-        uint8_t X;
-        uint8_t Y;
-        uint8_t SP;
-        uint16_t PC;
+        uint8_t A;      // Accumlator
+        uint8_t X;      // Index register 1
+        uint8_t Y;      // Index register 2
+        uint8_t SP;     // Stack pointer
+        uint16_t PC;    // Program counter
 
         uint8_t flags;
         Ram *ram;
