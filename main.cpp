f4eab757ce51903b785f3d3c9a358eb72c8d2c01
sabmeua <sabme.ua@gmail.com>
2019-09-03 07:51:42 +0000

    Use emulator device

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include "mbed.h"
 #include "Emu.h"
+#include "nesco/device/DiscoF746NG.h"
 
 using namespace nesco;
 
+device::EmuDevice_DiscoF746NG emu746ng;
+
 int main()
 {
-    Emu *emu = new Emu();
+    Emu *emu = new Emu(emu746ng);
     emu->run();
     return 0;
 }
