45365d7dd6b3cd6479a5c2672f4e5a0533870e71
sabmeua <sabme.ua@gmail.com>
2019-09-05 06:22:59 +0000

    Add virtual screen buffer explanation

--- nesco/include/Ppu.h
+++ nesco/include/Ppu.h
@@ -11,21 +11,72 @@ namespace nesco
      *
      * $0000 - $0FFF    Pattern table Low
      * $1000 - $1FFF    Pattern table High
-     * $2000 - $23BF    Name table 0
-     * $23C0 - $23FF    Attribute table 0
-     * $2400 - $27BF    Name table 1
-     * $27C0 - $27FF    Attribute table 1
-     * $2800 - $2BBF    Name table 2 (mirrored $2000-$23BF)
-     * $2BC0 - $2BFF    Attribute table 2 (mirroed $23C0-$23FF)
-     * $2C00 - $2FBF    Name table 3 (mirrored $2400-$27BF)
-     * $2FC0 - $2FFF    Attribute table 3 (mirroed $27C0-$2FFF)
+     * VRAM ----------------------------------------------------
+     * $2000 - $23BF    Screen 0 Name table
+     * $23C0 - $23FF    Screen 0 Attribute table
+     * $2400 - $27BF    Screen 1 Name table
+     * $27C0 - $27FF    Screen 1 Attribute table
+     * $2800 - $2BBF    Screen 2 Name table (mirrored $2000-$23BF)
+     * $2BC0 - $2BFF    Screen 2 Attribute table (mirroed $23C0-$23FF)
+     * $2C00 - $2FBF    Screen 3 Name table (mirrored $2400-$27BF)
+     * $2FC0 - $2FFF    Screen 3 Attribute table (mirroed $27C0-$2FFF)
+     * ---------------------------------------------------------
      * $3000 - $3EFF    Unused (mirrored $2000-$2EFF)
      * $3F00 - $3F0F    BG palette
-     * $3F10 - $3F1F    Sprite Palette
+     * $3F10 - $3F1F    Sprite palette
      * $3F20 - $3FFF    Unused (mirrored $3F00-$3F1F)
      * $4000 - $FFFF    Unused (mirrored $0000-$3FFF)
      *
      */
+
+    /**
+     * Virtual screen on VRAM
+     *
+     *   1 screen = 256 * 240 pixel
+     *   1 tile = 8 x 8 pixels -> 32 x 30 = 960 tiles
+     *   1 block = 2 x 2 blocks -> 16 x 15 = 240 blocks
+     *
+     *    | 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+     *    | 0 1 2 3 4 5 6 7 8 9 A B C D E F 0 1 2 3 4 5 6 7 8 9 A B C D E F
+     * ---+----------------------------------------------------------------
+     *  00|[]   <- 1tile: Each tiles are applied a sprite No.
+     *  20|
+     *  40|[  ] <- 1block(2x2 tiles): Each blocks are applied a BG No.
+     *  60|[  ]
+     *  80|
+     *  A0|
+     *  C0|
+     *  E0|
+     * 100|
+     * 120|
+     * 140|
+     * 160|
+     * 180|
+     * 1A0|
+     * 1C0|       Z <- if this tile shows character "Z" in screen 0 and
+     * 1E0|            the character ROM is mapped as below.
+     * 200|            0x5A is written in address 0x01C3.
+     * 220|
+     * 240|              char ROM $00 xx
+     * 260|                       $01 xx
+     * 280|                        ...
+     * 2A0|                       $41 "A"
+     * 2C0|                        ...
+     * 2E0|                       $5A "Z"
+     * 300|                        ...
+     * 320|                       $01 xx
+     * 340|
+     * 360|
+     * 380|
+     * 3A0|
+     *
+     */
+
+    typedef struct st_colors {
+        uint8_t r;
+        uint8_t g;
+        uint8_t b;
+    } Colors;
 
     class Ppu
     {
@@ -79,6 +130,7 @@ namespace nesco
         uint8_t PPUADDR;    // W:  PPU memory address register
         uint8_t PPUDATA;    // RW: PPU Memory data register
         uint8_t OAMDMA;     // W:  OAM DMA register (High byte)
+
     };
 };
 #endif
