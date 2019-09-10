6bfb2e1bd5691cced4372eac5da69f83216cc1a7
sabmeua <sabme.ua@gmail.com>
2019-09-10 07:12:08 +0000

    Add hal implementation of disco746ng

--- nesco/core/include/EmuDevice.h
+++ nesco/core/include/EmuDevice.h
@@ -1,6 +1,11 @@
 #ifndef NESCO_EMU_DEVICE_H
 #define NESCO_EMU_DEVICE_H
 
+#include "hal/include/Display.h"
+#include "hal/include/Sound.h"
+#include "hal/include/Filesystem.h"
+#include "hal/include/Keypad.h"
+
 namespace nesco::core
 {
     class EmuDevice
--- nesco/device/DiscoF746NG.cpp
+++ nesco/device/DiscoF746NG.cpp
@@ -1,7 +1,24 @@
+#include "mbed.h"
 #include "nesco/device/DiscoF746NG.h"
 
 namespace nesco::device
 {
+    class Display_DiscoF746NG : public hal::DisplayHal
+    {
+    } disp746ng;
+
+    class Sound_DiscoF746NG : public hal::SoundHal
+    {
+    } sound746ng;
+
+    class Filesystem_DiscoF746NG : public hal::FilesystemHal
+    {
+    } fs746ng;
+
+    class Keypad_DiscoF746NG : public hal::KeypadHal
+    {
+    } keypad746ng;
+
     void EmuDevice_DiscoF746NG::reset()
     {
     }
