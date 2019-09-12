71a0ab32601b46f18e069f2af5489f69f96926c7
sabmeua <sabme.ua@gmail.com>
2019-09-12 04:12:23 +0000

    Implement APU registers

--- nesco/core/include/Apu.h
+++ nesco/core/include/Apu.h
@@ -10,6 +10,48 @@ namespace nesco::core
     public:
         void writeRegister(uint16_t addr, uint8_t data);
         uint8_t readRegister(uint16_t addr);
+
+    private:
+        // Registers
+        union u_register {
+            // For access by name
+            struct {
+                // Pulse Ch 1 : $4000 ~ $4003
+                uint8_t PULSE1_CTRL1;   // W: Control register 1
+                uint8_t PULSE1_CTRL2;   // W: Control register 2
+                uint8_t PULSE1_FREQ1;   // W: Timer freq register 1
+                uint8_t PULSE1_FREQ2;   // W: Timer freq register 2
+                // Pulse Ch 2 : $4004 ~ $4007
+                uint8_t PULSE2_CTRL1;   // W: Control register 1
+                uint8_t PULSE2_CTRL2;   // W: Control register 2
+                uint8_t PULSE2_FREQ1;   // W: Timer freq register 1
+                uint8_t PULSE2_FREQ2;   // W: Timer freq register 2
+                // Triangle Ch : $4008 ~ $400B
+                uint8_t TRIANGLE_CTRL;  // W: Control register
+                uint8_t TRIANGLE_RESV;  // -: Reserved
+                uint8_t TRIANGLE_FREQ1; // W: Timer freq register 1
+                uint8_t TRIANGLE_FREQ2; // W: Timer freq register 2
+                // Noise Ch : $400C ~ $400F
+                uint8_t NOISE_CTRL;     // W: Control register
+                uint8_t NOISE_RESV;     // -: Reserved
+                uint8_t NOISE_FREQ1;    // W: Timer freq register 1
+                uint8_t NOISE_FREQ2;    // W: Timer freq register 2
+                // DMC Ch : $4010 ~ $4013
+                uint8_t DMC_CTRL1;      // W: Control register 1
+                uint8_t DMC_CTRL2;      // W: Control register 2
+                uint8_t DMC_ADDR;       // W: Sample address register
+                uint8_t DMC_LENGTH;     // W: Sample length register
+
+                uint8_t DUMMY_DMA;      // -: Padding 1byte : $4014
+                uint8_t SOUND_CTRL;     // RW: Sound control register : $4015
+                uint8_t DUMMY_PAD;      // -: Padding 1byte : $4016
+                uint8_t SOUND_FRAME;    // W: Sound frame register : $4017
+            } name;
+
+            // For access by index (index <- addr % 0x4000)
+            uint8_t index[0x18];
+        } Register;
+
     };
 };
 #endif
--- nesco/core/src/Apu.cpp
+++ nesco/core/src/Apu.cpp
@@ -5,11 +5,25 @@ namespace nesco::core
 
     void Apu::writeRegister(uint16_t addr, uint8_t data)
     {
+        uint16_t idx = addr % 0x4000;
+        if (idx <= 0x17 && (idx != 0x14 || idx != 0x16)) {
+            if (idx == 0x17) {
+                data &= 0xC0;
+            }
+            Register.index[idx] = data;
+        }
     }
     
     uint8_t Apu::readRegister(uint16_t addr)
     {
-        return 0;
+        uint8_t data = 0;
+        uint16_t idx = addr % 0x4000;
+        if (idx <= 0x17 && (idx != 0x14 || idx != 0x16)) {
+            data = Register.index[idx];
+        } else {
+            // Abort
+        }
+        return data;
     }
 
 };
