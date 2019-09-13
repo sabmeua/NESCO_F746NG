58707eee317e1e8474b977398871b6f094915ab5
sabmeua <sabme.ua@gmail.com>
2019-09-13 08:42:39 +0000

    Refactor APU implementation

--- nesco/core/include/Apu.h
+++ nesco/core/include/Apu.h
@@ -28,10 +28,47 @@ namespace nesco::core
         SequenceMode5Step,
     };
 
-    class PulseChannel;
-    class TriangleChannel;
-    class NoiseChannel;
-    class DmcChannel;
+    class ApuChannel
+    {
+    public:
+        void envelope() {};
+        void counter() {};
+        uint8_t readRegister(uint16_t addr) {
+            return Register.index[addr % 0x4000 % 4];
+        };
+        void writeRegister(uint16_t addr, uint8_t data) {
+            Register.index[addr % 0x4000 % 4] = data;
+        };
+    protected:
+        // Registers
+        union u_register {
+            // For access by name
+            struct {
+                uint8_t CTRL1;  // W: Control register 1
+                uint8_t CTRL2;  // W: Control register 2
+                uint8_t FREQ1;  // W: Timer freq register 1
+                uint8_t FREQ2;  // W: Timer freq register 2
+            } name;
+            // For access by index (index <- addr % 0x4000 % 4)
+            uint8_t index[4];
+        } Register;
+    };
+
+    class PulseChannel : public ApuChannel
+    {
+    };
+
+    class TriangleChannel : public ApuChannel
+    {
+    };
+
+    class NoiseChannel : public ApuChannel
+    {
+    };
+
+    class DmcChannel : public ApuChannel
+    {
+    };
 
     class Apu
     {
@@ -42,57 +79,17 @@ namespace nesco::core
         uint8_t readRegister(uint16_t addr);
 
     private:
-        friend class PulseChannel;
         PulseChannel *pulse1;
         PulseChannel *pulse2;
-        friend class TriangleChannel;
         TriangleChannel *triangle;
-        friend class NoiseChannel;
         NoiseChannel *noise;
-        friend class DmcChannel;
         DmcChannel *dmc;
 
         uint8_t cycle;
 
         // Registers
-        union u_register {
-            // For access by name
-            struct {
-                // Pulse Ch 1 : $4000 ~ $4003
-                uint8_t PULSE1_CTRL1;   // W: Control register 1
-                uint8_t PULSE1_CTRL2;   // W: Control register 2
-                uint8_t PULSE1_FREQ1;   // W: Timer freq register 1
-                uint8_t PULSE1_FREQ2;   // W: Timer freq register 2
-                // Pulse Ch 2 : $4004 ~ $4007
-                uint8_t PULSE2_CTRL1;   // W: Control register 1
-                uint8_t PULSE2_CTRL2;   // W: Control register 2
-                uint8_t PULSE2_FREQ1;   // W: Timer freq register 1
-                uint8_t PULSE2_FREQ2;   // W: Timer freq register 2
-                // Triangle Ch : $4008 ~ $400B
-                uint8_t TRIANGLE_CTRL;  // W: Control register
-                uint8_t TRIANGLE_RESV;  // -: Reserved
-                uint8_t TRIANGLE_FREQ1; // W: Timer freq register 1
-                uint8_t TRIANGLE_FREQ2; // W: Timer freq register 2
-                // Noise Ch : $400C ~ $400F
-                uint8_t NOISE_CTRL;     // W: Control register
-                uint8_t NOISE_RESV;     // -: Reserved
-                uint8_t NOISE_FREQ1;    // W: Timer freq register 1
-                uint8_t NOISE_FREQ2;    // W: Timer freq register 2
-                // DMC Ch : $4010 ~ $4013
-                uint8_t DMC_CTRL1;      // W: Control register 1
-                uint8_t DMC_CTRL2;      // W: Control register 2
-                uint8_t DMC_ADDR;       // W: Sample address register
-                uint8_t DMC_LENGTH;     // W: Sample length register
-
-                uint8_t DUMMY_DMA;      // -: Padding 1byte : $4014
-                uint8_t SOUND_CTRL;     // RW: Sound control register : $4015
-                uint8_t DUMMY_PAD;      // -: Padding 1byte : $4016
-                uint8_t SOUND_FRAME;    // W: Sound frame register : $4017
-            } name;
-
-            // For access by index (index <- addr % 0x4000)
-            uint8_t index[0x18];
-        } Register;
+        uint8_t SOUND_CTRL;     // RW: Sound control register : $4015
+        uint8_t SOUND_FRAME;    // W: Sound frame register : $4017
 
         void counter();
         void envelope();
--- nesco/core/src/Apu.cpp
+++ nesco/core/src/Apu.cpp
@@ -2,47 +2,13 @@
 
 namespace nesco::core
 {
-    class ApuChannel
-    {
-    public:
-        ApuChannel(Apu *_apu) : apu(_apu) {};
-        void envelope() {};
-        void counter() {};
-    private:
-        Apu *apu;
-    };
-
-    class PulseChannel : public ApuChannel
-    {
-    public:
-        PulseChannel(Apu *_apu) : ApuChannel(_apu) {};
-    };
-
-    class TriangleChannel : public ApuChannel
-    {
-    public:
-        TriangleChannel(Apu *_apu) : ApuChannel(_apu) {};
-    };
-
-    class NoiseChannel : public ApuChannel
-    {
-    public:
-        NoiseChannel(Apu *_apu) : ApuChannel(_apu) {};
-    };
-
-    class DmcChannel : public ApuChannel
-    {
-    public:
-        DmcChannel(Apu *_apu) : ApuChannel(_apu) {};
-    };
-
     Apu::Apu()
     {
-        pulse1 = new PulseChannel(this);
-        pulse2 = new PulseChannel(this);
-        triangle = new TriangleChannel(this);
-        noise = new NoiseChannel(this);
-        dmc = new DmcChannel(this);
+        pulse1 = new PulseChannel();
+        pulse2 = new PulseChannel();
+        triangle = new TriangleChannel();
+        noise = new NoiseChannel();
+        dmc = new DmcChannel();
         cycle = 0;
     }
 
@@ -79,12 +45,20 @@ namespace nesco::core
 
     void Apu::writeRegister(uint16_t addr, uint8_t data)
     {
-        uint16_t idx = addr % 0x4000;
-        if (idx <= 0x17 && (idx != 0x14 || idx != 0x16)) {
-            if (idx == 0x17) {
-                data &= 0xC0;
-            }
-            Register.index[idx] = data;
+        if (addr <= 0x4003) {
+            pulse1->writeRegister(addr, data);
+        } else if (addr <= 0x4007) {
+            pulse2->writeRegister(addr, data);
+        } else if (addr <= 0x400B) {
+            triangle->writeRegister(addr, data);
+        } else if (addr <= 0x400F) {
+            noise->writeRegister(addr, data);
+        } else if (addr <= 0x4013) {
+            dmc->writeRegister(addr, data);
+        } else if (addr == 0x4015) {
+            SOUND_CTRL = data;
+        } else if (addr == 0x4017) {
+            SOUND_FRAME = data & 0xC0;
         }
     }
     
@@ -92,14 +66,14 @@ namespace nesco::core
     {
         uint8_t data = 0;
         if (addr == 0x4015) {
-            data = Register.index[addr % 0x4000];
+            data = SOUND_CTRL;
         }
         return data;
     }
 
     ApuFrameSequenceMode Apu::getFrameSequenceMode()
     {
-        return static_cast<ApuFrameSequenceMode>(Register.name.SOUND_CTRL / 0x80);
+        return static_cast<ApuFrameSequenceMode>(SOUND_CTRL / 0x80);
     }
 
     void Apu::envelope()
