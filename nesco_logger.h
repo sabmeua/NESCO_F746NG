d4797a309bf38c92c7fd94d684c34464221b3a43
sabmeua <sabme.ua@gmail.com>
2019-09-20 08:34:25 +0000

    Add logging disable option

--- nesco_logger.h
+++ nesco_logger.h
@@ -3,17 +3,30 @@
 
 #include "nesco_config.h"
 
-#if defined NESCO_PLATFORM_GLUT
-    #include "DefaultLogger.h"
-    typedef DefaultLogger Logger;
-#elif defined NESCO_PLATFORM_MBED
-    #include "DefaultLogger.h"
-    typedef DefaultLogger Logger;
-#endif
+//#define LOG_DISABLE
 
-#define LOG_ERROR(...) Logger::puts(LogLvError, __VA_ARGS__)
-#define LOG_WARNING(...) Logger::puts(LogLvWarning, __VA_ARGS__)
-#define LOG_INFO(...) Logger::puts(LogLvInfo, __VA_ARGS__)
-#define LOG_DEBUG(...) Logger::puts(LogLvDebug, __VA_ARGS__)
+#ifndef LOG_DISABLE
+
+    #if defined NESCO_PLATFORM_GLUT
+        #include "DefaultLogger.h"
+        typedef DefaultLogger Logger;
+    #elif defined NESCO_PLATFORM_MBED
+        #include "DefaultLogger.h"
+        typedef DefaultLogger Logger;
+    #endif
+
+    #define LOG_ERROR(...) Logger::puts(LogLvError, __VA_ARGS__)
+    #define LOG_WARNING(...) Logger::puts(LogLvWarning, __VA_ARGS__)
+    #define LOG_INFO(...) Logger::puts(LogLvInfo, __VA_ARGS__)
+    #define LOG_DEBUG(...) Logger::puts(LogLvDebug, __VA_ARGS__)
+
+#else
+
+    #define LOG_ERROR(...)
+    #define LOG_WARNING(...)
+    #define LOG_INFO(...)
+    #define LOG_DEBUG(...)
+
+#endif
 
 #endif
