f2cf746d32d079e59b6c9aba3107ea3b5453af41
sabmeua <sabme.ua@gmail.com>
2019-09-27 02:30:27 +0000

    Use callback for emu mainloop

--- nesco/core/include/EmuDevice.h
+++ nesco/core/include/EmuDevice.h
@@ -1,6 +1,8 @@
 #ifndef NESCO_CORE_EMU_DEVICE_H
 #define NESCO_CORE_EMU_DEVICE_H
 
+#include <functional>
+
 #include "hal/include/DisplayHal.h"
 #include "hal/include/SoundHal.h"
 #include "hal/include/FilesystemHal.h"
@@ -24,6 +26,19 @@ namespace nesco::core
         SoundHal *sound;
         FilesystemHal *filesystem;
         KeypadHal *keypad;
+
+        void setCallback(std::function<void(void)> _func) {
+            mainloop = _func;
+        }
+
+        void main() {
+            int i = 0;
+            while(i++ < 100) {
+                mainloop();
+            }
+        }
+    protected:
+        std::function<void(void)> mainloop;
     };
 
 };
